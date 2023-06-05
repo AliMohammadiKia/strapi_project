c7f20e6594a99487d5a16a85144b7056b28208e5
AliMohammadiKia <AliMohammadiKia01@gmail.com>
2023-06-05 18:58:04 +0330

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-center mt-5 text-indigo-600 font-bold">
+        Hello World!
+      </h1>
+    </div>
+  );
+};
+
+export default App;
