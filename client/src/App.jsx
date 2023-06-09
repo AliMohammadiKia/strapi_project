170d99c79c3c9002618c6de0f5e5cf32791f502a
AliMohammadiKia <AliMohammadiKia01@gmail.com>
2023-06-09 11:49:19 +0330

    add useThemeDetector custom hook

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,18 @@
 import Router from "./routes/Router";
+import useThemeDetector from "./hooks/useThemeDetector";
+import { useEffect } from "react";
 
 const App = () => {
+  const isDarkTheme = useThemeDetector();
+
+  useEffect(() => {
+    if (isDarkTheme) {
+      document.documentElement.setAttribute("data-theme", "dark");
+    } else {
+      document.documentElement.setAttribute("data-theme", "light");
+    }
+  }, [isDarkTheme]);
+
   return <Router />;
 };
 
