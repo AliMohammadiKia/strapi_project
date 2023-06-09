3e6d3a1670c55b5cdd9dfb978eb2dc84962960c9
AliMohammadiKia <AliMohammadiKia01@gmail.com>
2023-06-09 11:35:41 +0330

    install daisy ui

--- client/src/components/Header/Greeting.jsx
+++ client/src/components/Header/Greeting.jsx
@@ -1,8 +1,8 @@
 const Greeting = () => {
   return (
     <div className="flex-1">
-      <p className="text-3xl font-medium text-gray-800">Welcome, Ali</p>
-      <p className="text-gray-500 pt-2">Discover whatever you need easily</p>
+      <p className="text-3xl font-medium text-base-800">Welcome, Ali</p>
+      <p className="text-base-500 pt-2">Discover whatever you need easily</p>
     </div>
   );
 };
