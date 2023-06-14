3e9bd713fe63150a6763f55f93d974b89ec6dc24
AliMohammadiKia <AliMohammadiKia01@gmail.com>
2023-06-14 15:23:03 +0330

    handle click for active category item

--- client/src/components/CategoryItem/CategoryItem.jsx
+++ client/src/components/CategoryItem/CategoryItem.jsx
@@ -1,19 +1,12 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useEffect, useState } from "react";
 
-const CategoryItem = ({ text, icon, id, selectedItem = 1 }) => {
-  const [activeItem, setActiveItem] = useState(selectedItem);
-
-  // useEffect(() => {
-
-  // }, [activeItem]);
-
+const CategoryItem = ({ text, icon, id, handleClick, selectedItem = 1 }) => {
   return (
     <button
       className={`btn ${
-        id === activeItem ? "btn-active" : "btn-outline"
+        id === selectedItem ? "btn-active" : "btn-outline"
       } btn-error gap-2 px-6`}
-      onClick={() => setActiveItem(id)}
+      onClick={() => handleClick(id)}
     >
       <FontAwesomeIcon icon={icon} className="text-lg" />
       {text ? text : "Category"}
--- client/src/pages/Home/CategoryItems.jsx
+++ client/src/pages/Home/CategoryItems.jsx
@@ -6,10 +6,12 @@ import {
   faIceCream,
 } from "@fortawesome/free-solid-svg-icons";
 import useFetchData from "../../hooks/useFetchData";
+import { useState } from "react";
 
 const CategoryItems = () => {
   const { data, error, loading } = useFetchData("/categories");
   const icons = [faMugHot, faStroopwafel, faBreadSlice, faIceCream];
+  const [activeItem, setActiveItem] = useState(1);
 
   return (
     <div className="mt-8 flex gap-5">
@@ -26,6 +28,8 @@ const CategoryItems = () => {
             text={category.attributes.name}
             icon={icons[index]}
             id={category.id}
+            handleClick={(id) => setActiveItem(id)}
+            selectedItem={activeItem}
           />
         ))
       )}
